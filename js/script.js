fe97926a7d57be390b6f270310392323d035880a
scavesvor <64730041+scavesvor@users.noreply.github.com>
2022-06-26 20:20:11 +0400

    Update script.js
    a shorter and neater way to write the code although I left out the function to show the error image.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,59 @@ const formEl = document.getElementById("form");
 const errorText = document.querySelectorAll(".intro__right__form__alert");
 const errorImg = document.querySelectorAll(".intro__right__form__alert--img");
 
+//changes start
+
+const nameEl = document.getElementById("name");
+const surnameEl = document.getElementById("surname");
+const emailEl = document.getElementById("email");
+const passwordEl = document.getElementById("password");
+const formEl = document.getElementById("form");
+const errorText = document.querySelectorAll(".intro_rightform_alert");
+
+
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  for (let i = 0; i < errorText.length; i++) {
+      if (nameEl.value === ""){
+        errorText[0].classList.add("active");
+        nameEl.style.border = "1px solid red"
+         
+        setInterval(function() {
+        errorText[0].classList.remove("active");
+        nameEl.style.border = "";
+        }, 3000);    
+      } else if (surnameEl.value == ""){
+          errorText[1].classList.add("active");
+        surnameEl.style.border = "1px solid red"
+        
+        setInterval(function() {
+        errorText[1].classList.remove("active");
+        surnameEl.style.border = "";
+        }, 3000);
+      } else if (emailEl.value == ""){
+          errorText[2].classList.add("active");
+        emailEl.style.border = "1px solid red"
+        
+        setInterval(function() {
+        errorText[2].classList.remove("active");
+        emailEl.style.border = "";
+        }, 3000);
+      } else if (passwordEl.value == ""){
+          errorText[3].classList.add("active");
+        passwordEl.style.border = "1px solid black"
+        
+        setInterval(function() {
+        errorText[3].classList.remove("active");
+        passwordEl.style.border = "";
+        }, 3000);
+      } 
+      
+}});
+
+// changes end
+
+
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   for (let i = 0; i < errorText.length; i++) {
